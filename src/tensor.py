496e4a339320fa78ecf5092704f8605fb524945b
pedronovaes <pedronovaesmelo@gmail.com>
2025-03-28 11:33:17 -0300

    Implement backward function to calculate derivatives instead of using a manual approach

--- src/tensor.py
+++ src/tensor.py
@@ -26,10 +26,24 @@ class Value:
     def __add__(self, other: Value) -> Value:
         output = Value(data=self.data + other.data, _children=(self, other))
 
+        # Compute gradients. An add operator is just a distributor of
+        # gradients (the children nodes receives the same father's gradient).
+        def _backward():
+            self.grad += output.grad
+            other.grad += output.grad
+
+        output._backward = _backward
+
         return output
 
     def __mul__(self, other: Value) -> Value:
         output = Value(data=self.data * other.data, _children=(self, other))
+
+        def _backward():
+            self.grad += other.data * output.grad
+            other.grad += self.data * output.grad
+
+        output._backward = _backward
 
         return output
 
@@ -37,5 +51,10 @@ class Value:
     def tanh(self) -> Value:
         tan = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
         output = Value(data=tan, _children=(self,))
+
+        def _backward():
+            self.grad += (1 - tan ** 2) * output.grad
+
+        output._backward = _backward
 
         return output
